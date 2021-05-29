bd7277c614be181dcf41498c78e90ab54b4edc81
jinxy <70546159+JinxGT@users.noreply.github.com>
2021-05-29 08:53:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const prefix = ".."
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} :)`);
+    
+    bot.channels.cache.get('848030787889528832').send(`<:Green:845805015108812810> Bot Online`)
+    
     bot.user.setActivity(`..help In ${bot.guilds.cache.size} Servers!`, {
         type: "WATCHING",
         url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
