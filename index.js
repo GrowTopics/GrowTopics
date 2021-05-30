fd151b00e2a05593757f8e1145a2207c8587b10d
jinxy <70546159+JinxGT@users.noreply.github.com>
2021-05-30 15:11:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.on('ready', () => {
     bot.channels.cache.get('848030787889528832').send(`I have restarted!`)
     bot.channels.cache.get('848662028343836682').send(`No errors, I have restarted!`)
     
-    bot.user.setActivity(`..help In ${bot.guilds.cache.size} Servers!`, {
+    bot.user.setActivity(`${bot.guilds.cache.size} Servers | beta-1.0.0`, {
         type: "WATCHING",
         url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
     });
@@ -20,14 +20,14 @@ bot.on('ready', () => {
 )
 
 bot.on("guildCreate", guild => {
-bot.user.setActivity(`..help In ${bot.guilds.cache.size} Servers!`, { 
+bot.user.setActivity(`${bot.guilds.cache.size} Servers | beta-1.0.0`, { 
         type: "WATCHING", 
         url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
     });
 })
 
 bot.on("guildDelete", guild => {
-bot.user.setActivity(`..help In ${bot.guilds.cache.size} Servers!`, { 
+bot.user.setActivity(`${bot.guilds.cache.size} Servers | beta-1.0.0`, { 
         type: "WATCHING", 
         url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
     });
